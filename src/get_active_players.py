05c382a2f90f10e72b415c39d4fc06f5e6d0d75d
piotrb <piotrbanko@me.com>
2023-12-04 15:30:37 +0100

    Added functions to create a list of active players

--- src/get_active_players.py
+++ src/get_active_players.py
@@ -1,6 +1,8 @@
 """Get addresses of players that have been active in the last X days"""
 import configparser
+import datetime
 import requests
+import pandas as pd
 
 api_config = configparser.ConfigParser()
 api_config.read('../data/api_keys.ini')
@@ -42,3 +44,70 @@ def get_txs(endblock: int) -> list:
 
     return txs_list
 
+
+def get_block_number_by_timestamp(timestamp: int) -> int:
+    """
+    Get BSC block number
+    :param timestamp: Timestamp to get block number for
+    :return: BSC block number
+    """
+    url = f"https://api.bscscan.com/api?module=block&action=getblocknobytime&timestamp={timestamp}&closest=" \
+          f"before&apikey={bscscan_api_key}"
+
+    response = requests.request("GET", url)
+
+    block = response.json()
+    block = block['result']
+
+    return int(block)
+
+
+def save_active_players_to_csv(df: pd.DataFrame, active_players_file: str) -> None:
+    """
+    Save dataframe to csv file
+    :param df: Dataframe to save
+    :param active_players_file: Directory to save the file to
+    :return: None
+    """
+    df.to_csv(active_players_file, header=True, index=False)
+
+
+def make_active_players_file(timestamp_from: int, timestamp_to: int, number_of_days_to_check: int,
+                             active_players_file: str) -> None:
+    """
+    Make active players CSV file
+    :param timestamp_from: Timestamp to select the txs from
+    :param timestamp_to: Timestamp to select the txs to
+    :param number_of_days_to_check: Number of days to check - starting from current day block number backwards
+    :param active_players_file: Directory to save the file to
+    :return: None
+    """
+    txs = []
+
+    block_number = get_block_number_by_timestamp(timestamp_to)
+
+    for i in range(number_of_days_to_check):
+        txs_part = get_txs(block_number - i * 28233)  # subtract number of daily blocks
+        txs.extend(txs_part)
+
+    df = pd.DataFrame(txs)
+
+    df.sort_values('timestamp')
+    print(f"Current range of downloaded data: {datetime.datetime.fromtimestamp(df['timestamp'].min())}"
+          f" - {datetime.datetime.fromtimestamp(df['timestamp'].max())}")
+    print(f"Selecting records within {datetime.datetime.fromtimestamp(timestamp_from)}"
+          f" - {datetime.datetime.fromtimestamp(timestamp_to)}")
+
+    df = df[df['timestamp'] >= float(timestamp_from)]
+    df = df[df['timestamp'] <= float(timestamp_to)]
+
+    before_dropping_duplicates_size = df.shape[0]
+    df = df.drop_duplicates(keep='first')
+    after_dropping_duplicates_size = df.shape[0]
+
+    print(f"Removed {before_dropping_duplicates_size - after_dropping_duplicates_size} duplicates")
+
+    counts_df = df['player'].value_counts().reset_index()
+    counts_df.columns = ['player', 'count']
+
+    save_active_players_to_csv(counts_df, active_players_file)
